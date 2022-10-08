568e9d42b59dec244a0b750bec1a7b78904faf64
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2022-10-07 23:54:12 -0300

    Updating policies and targets groups
    Updating policies and targets groups

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-ecs"
+    bucket = "terraform-state-docker-ecs"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
   }
--- env/prod/main.tf
+++ env/prod/main.tf
@@ -2,4 +2,9 @@ module "prod" {
     source = "../../infra"
 
     nome_repositorio = "producao"
+    cargoiam = "producao"
+}
+
+output "IP_alb" {
+  value = module.prod.IP
 }
